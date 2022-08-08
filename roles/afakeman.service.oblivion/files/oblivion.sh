1ec57348abf6d90a57fd50e6e0c8bf9d856d5e22
Anton Suslov <anton_suslov@me.com>
2022-08-08 21:59:54 +0300

    Added oblivion service

---
+++ roles/afakeman.service.oblivion/files/oblivion.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+
+set -eauo pipefail
+
+title="Даванем подливы"
+
+function random_voiceline() {
+    sort -R cgi-bin/oblivion_dialogue.txt | head -n1 | cut -d'@' -f1
+}
+
+if [ -z "$PATH_INFO" ] || [ "$PATH_INFO" = "/" ]; then
+    dialogueid=`random_voiceline || true`
+else
+    dialogueid="${PATH_INFO:1}"
+fi
+
+raw_line=`grep "^$dialogueid@" cgi-bin/oblivion_dialogue.txt || true`
+
+echo -en 'Content-type: text/html; charset=UTF-8\r\n'
+echo -en '\r\n'
+
+echo "<html>"
+echo "<head>"
+echo "<title>$title</title>"
+echo "</head>"
+echo "<body>"
+if [ -z "$raw_line" ]; then
+    echo "<h1>Я ваще не ебу о чем ты</h1>"
+    echo "<a href=\"/\">еще</h1>"
+else
+    line=`echo "$raw_line" | cut -d'@' -f2`
+    echo "<h1>$line</h1>"
+    echo "<a href=\"$dialogueid\">пермалинк</h1>"
+    echo "<br>"
+    echo "<a href=\"/\">еще</h1>"
+fi
+echo "</body>"
+echo "</html>"
