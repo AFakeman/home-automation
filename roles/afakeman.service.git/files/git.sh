47683db4a8881e074ef0ec0ea42494849e14541b
Anton Suslov <anton_suslov@me.com>
2022-08-14 21:25:24 +0300

    Add public/private git repo separation

--- roles/afakeman.service.git/files/git.sh
+++ roles/afakeman.service.git/files/git.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 export CGIT_CONFIG=/usr/local/etc/cgitrc
 export SCRIPT_NAME=
+export MODE="${HTTP_USER_AGENT}"
+
+if [ "$HTTP_USER_AGENT" = "local" ]; then
+    export MODE=all
+else
+    export MODE=public
+fi
+
+echo -en "X-Is-Remote: $MODE\r\n"
 exec /usr/local/opt/cgit/share/cgit/cgit.cgi
